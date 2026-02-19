ebe06e378c601483decb7f8c47a58b37cc2cf6d0
tohird03 <tohirjondoniyorov@gmail.com>
2026-02-19 22:09:59 +0500

    Add minPercent to category

--- src/api/category/types.ts
+++ src/api/category/types.ts
@@ -2,10 +2,12 @@ export interface ICategory {
   id: string,
   name: string,
   percent: number,
+  minPercent: number,
   createdAt: string,
 }
 
 export interface IEditCategory {
   id: string,
   percent: number,
+  minPercent: number,
 }
